e329673e117c178f98f32fcf6a98df2bad8506c3
kirtiTiwari26 <39180806+kirtiTiwari26@users.noreply.github.com>
2018-05-11 12:57:09 +0530

    Delivery Vendor Activity

---
+++ app/src/main/java/com/yberry/dinehawaii/vendor/Activity/DeliveryVendorDetailActivity.java
@@ -0,0 +1,15 @@
+package com.yberry.dinehawaii.vendor.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yberry.dinehawaii.R;
+
+public class DeliveryVendorDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_vendor_detail);
+    }
+}
